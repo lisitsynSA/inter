5dfbb3ca1715316b44057c398b3105b8b6feb3c5
Sergey Lisitsyn <fox9508@ya.ru>
2017-02-02 17:40:29 +0300

    [HOST] [FEAT] Add ddk to QT Project

--- HOST_QT/mainwindow.h
+++ HOST_QT/mainwindow.h
@@ -15,8 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+protected:
+    //closeEvent(QCloseEvent *event);
+
+private slots:
+    void start_hid();
+
 private:
     Ui::MainWindow *ui;
+    int hid_open();
+    int rawhid_open(int max, int vid, int pid, int usage_page, int usage);
 };
 
 #endif // MAINWINDOW_H
